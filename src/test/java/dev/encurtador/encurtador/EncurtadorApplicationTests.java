99b8edbaa2ddb790a26d74422412d55d6d30bf8d
LuanArves <luanspsoares@outlook.com>
2024-07-23 17:24:28 -0300

    primeiro commit

---
+++ src/test/java/dev/encurtador/encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.encurtador.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
